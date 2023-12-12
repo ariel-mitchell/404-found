b95dbbea442538559f14ca05b8318bc71445f0b7
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-11 22:54:51 -0600

    PostController handles HTTP requests (posts), uses PostSerivce to perform

--- DD-Creator/src/main/java/controllers/PostController.java
+++ DD-Creator/src/main/java/controllers/PostController.java
@@ -1,4 +1,26 @@
 package controllers;
 
+import models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import service.PostService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
 }
