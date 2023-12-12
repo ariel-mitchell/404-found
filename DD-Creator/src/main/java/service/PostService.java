6edbde4c648ca98c891726ca618928ba27b717ed
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-11 20:36:30 -0600

    Learned about Service in relation to Blogs. Handling Post Features. Retrieve all posts

--- DD-Creator/src/main/java/service/PostService.java
+++ DD-Creator/src/main/java/service/PostService.java
@@ -1,4 +1,21 @@
 package service;
 
+import models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.PostRepository;
+
+import java.util.List;
+
+//Service for managing operations related to Post entity
+@Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    //Method to retrieve all posts
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
 }
