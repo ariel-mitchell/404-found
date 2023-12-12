89890aa184a92ce4d5c391e8ef05acdfca9b35af
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-11 20:34:56 -0600

    Finished User/Post Repository for now

--- DD-Creator/src/main/java/repository/PostRepository.java
+++ DD-Creator/src/main/java/repository/PostRepository.java
@@ -3,8 +3,10 @@ package repository;
 
 import models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // interface for handling Post entity in database
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
--- DD-Creator/src/main/java/repository/UserRepository.java
+++ DD-Creator/src/main/java/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package repository;
 
-public interface UserRepository {
+import models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
