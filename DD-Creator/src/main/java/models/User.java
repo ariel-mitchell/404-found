6c6f9e59d904907c79695cd215f7712eb824069a
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-11 18:30:37 -0600

    Blog - User

--- DD-Creator/src/main/java/models/User.java
+++ DD-Creator/src/main/java/models/User.java
@@ -16,4 +16,42 @@ public class User {
     String email;
     private String password;
 
+    public User() {
+    }
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    //getters and setters for User field
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
