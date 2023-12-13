d8ad614a5f72e62b972379f84de78e42c11f0804
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-12 20:48:13 -0600

    User/PostController - basic functionality to test blog features

--- DD-Creator/src/main/java/controllers/PostController.java
+++ DD-Creator/src/main/java/controllers/PostController.java
@@ -4,6 +4,7 @@ import models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import service.PostService;
+import java.util.List;
 
 import java.util.List;
 
--- DD-Creator/src/main/java/controllers/UserController.java
+++ DD-Creator/src/main/java/controllers/UserController.java
@@ -1,5 +1,23 @@
 package controllers;
 
+import models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    //endpoint for creating new user
+    @RequestMapping("/register")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+    //other endpoints?
 }
