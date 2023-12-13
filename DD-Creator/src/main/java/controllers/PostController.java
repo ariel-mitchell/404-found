538be9830c2357aa277f91470b3da6e74ba6975e
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-12 22:36:24 -0600

    Get All Post, Get Post by {id}

--- DD-Creator/src/main/java/controllers/PostController.java
+++ DD-Creator/src/main/java/controllers/PostController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import service.PostService;
@@ -11,9 +12,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+    private final PostService postService;
 
     @Autowired
-    private PostService postService;
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+    //Get All Post
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts(){
+        List<Post> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
+    }
+    //Get Post by {id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPostById(@PathVariable Long id) {
+        Post post = postService.getPostById(id);
+        return ResponseEntity.ok(post);
+    }
+
 
     // Endpoint to create a new post via HTTP POST request
     @PostMapping("/posts/create")
