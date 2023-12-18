9e462c2d0d11496e2d86da723b58da64507cce2c
Jweiler94 <jweiler94@hotmail.com>
2023-12-17 19:27:49 -0600

    finished controllers for existing models, characters, and search. Finished search functionality

--- DD-Creator/src/main/java/controllers/HomeController.java
+++ DD-Creator/src/main/java/controllers/HomeController.java
@@ -1,9 +1,136 @@
 package controllers;
 
+import jakarta.validation.Valid;
+import models.*;
+import models.Character;
+import models.Data.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/character")
 public class HomeController {
+
+    @Autowired
+    private CharacterRepository characterRepository;
+
+    @Autowired
+    private AlignmentRepository alignmentRepository;
+
+    @Autowired
+    private BackgroundRepository backgroundRepository;
+
+    @Autowired
+    private ClassRepository classRepository;
+
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired
+    private ProficiencyRepository proficiencyRepository;
+
+    @Autowired
+    private RaceRepository raceRepository;
+
+    @Autowired
+    private SpellRepository spellRepository;
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "All Characters");
+        List<Character> characters = (List<Character>) characterRepository.findAll();
+        model.addAttribute("characters", characters);
+        return "index";
+    }
+
+    @GetMapping("add")
+    public String displayAddCharacterForm(Model model) {
+        model.addAttribute("title", "Add Character");
+        model.addAttribute(new Character());
+
+        List<Alignment> alignment = (List<Alignment>) alignmentRepository.findAll();
+        model.addAttribute("alignment", alignment);
+
+        List<Background> background = (List<Background>) backgroundRepository.findAll();
+        model.addAttribute("background", background);
+
+        List<ClassInfo> classInfo = (List<ClassInfo>) classRepository.findAll();
+        model.addAttribute("class info", classInfo);
+
+        List<Equipment> equipment = (List<Equipment>) equipmentRepository.findAll();
+        model.addAttribute("equipment", equipment);
+
+        List<Proficiencies> proficiencies = (List<Proficiencies>) proficiencyRepository.findAll();
+        model.addAttribute("proficiencies", proficiencies);
+
+        List<Race> race = (List<Race>) raceRepository.findAll();
+        model.addAttribute("race", race);
+
+        List<Spells> spells = (List<Spells>) spellRepository.findAll();
+        model.addAttribute("spells", spells);
+
+        return "add";
+    }
+
+    @PostMapping("add")
+    public String processAddCharacterForm(@ModelAttribute @Valid Character newCharacter, Errors errors, Model model, @RequestParam int alignmentId, @RequestParam int backgroundId, @RequestParam int classId, @RequestParam List<Integer> equipment, @RequestParam List<Integer> proficiencies, @RequestParam int raceId, @RequestParam List<Integer> spells) {
+
+        if(errors.hasErrors()) {
+            return "add";
+        }
+
+        Optional<Alignment> optionalAlignment = alignmentRepository.findById(alignmentId);
+        if (optionalAlignment.isPresent()) {
+            Alignment alignment = optionalAlignment.get();
+            newCharacter.setAlignment(alignment);
+        }
+
+        Optional<ClassInfo> optionalClassInfo = classRepository.findById(classId);
+        if (optionalClassInfo.isPresent()) {
+            ClassInfo classInfo = optionalClassInfo.get();
+            newCharacter.setClassInfo(classInfo);
+        }
+
+        Optional<Race> optionalRace = raceRepository.findById(raceId);
+        if (optionalRace.isPresent()) {
+            Race race = optionalRace.get();
+            newCharacter.setRace(race);
+        }
+
+        Optional<Background> optionalBackground = backgroundRepository.findById(backgroundId);
+        if (optionalBackground.isPresent()) {
+            Background background = optionalBackground.get();
+            newCharacter.setBackground(background);
+        }
+
+        List<Equipment> equipmentObjs = (List<Equipment>) equipmentRepository.findAllById(equipment);
+        newCharacter.setEquipment(equipmentObjs);
+
+        List<Proficiencies> proficienciesObjs = (List<Proficiencies>) proficiencyRepository.findAllById(proficiencies);
+        newCharacter.setProficiencies(proficienciesObjs);
+
+        List<Spells> spellObjs = (List<Spells>) spellRepository.findAllById(spells);
+        newCharacter.setSpells(spellObjs);
+
+        characterRepository.save(newCharacter);
+        return "redirect:";
+    }
+
+    @GetMapping("view/{characterId}")
+    public String displayViewCharacter(Model model, @PathVariable int characterId) {
+        Optional<Character> optionalCharacter = characterRepository.findById(characterId);
+        if(optionalCharacter.isPresent()) {
+            Character character = (Character) optionalCharacter.get();
+            model.addAttribute("character", character);
+            return "view";
+        } else {
+            return "redirect:./";
+        }
+    }
 }
--- DD-Creator/src/main/java/controllers/SpellController.java
+++ DD-Creator/src/main/java/controllers/SpellController.java
@@ -1,4 +1,57 @@
 package controllers;
 
+import jakarta.validation.Valid;
+import models.Data.SpellRepository;
+import models.Spells;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("spells")
 public class SpellController {
+
+    @Autowired
+    private SpellRepository spellRepository;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("spells", spellRepository.findAll());
+        return "spells/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddSpellForm(Model model) {
+        model.addAttribute(new Spells());
+        return "spells/add";
+    }
+
+    @GetMapping("add")
+    public String processAddSpellForm(@ModelAttribute @Valid Spells newSpell, Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            return "spells/add";
+        }
+        spellRepository.save(newSpell);
+        return "redirect:./";
+    }
+
+    @GetMapping("view/{spellId}")
+    public String displayViewSpells(Model model, @PathVariable int spellId) {
+
+        Optional<Spells> optionalSpell = spellRepository.findById(spellId);
+        if (optionalSpell.isPresent()) {
+            Spells spell = optionalSpell.get();
+            model.addAttribute("spell", spell);
+            return "spells/view";
+        } else {
+            return "redirect:../";
+        }
+    }
 }
