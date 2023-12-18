7f2e30fb3ae00e06059e748e512b9ac95d3c54ac
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-17 19:24:07 -0600

    Edit App for testing. Remove empty files

---
+++ DDUI/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import PostList from './PostList';
+import CreatePost from './CreatePost';
+
+function App() {
+return (
+    <div className="App">
+    <h1>Blog App</h1>
+    <CreatePost />
+    <PostList />
+    </div>
+);
+}
+
+export default App;
