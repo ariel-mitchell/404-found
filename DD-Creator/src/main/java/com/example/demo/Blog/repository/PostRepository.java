b16a8a0114820fe4306ea19fda688d5991f190aa
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-18 00:43:52 -0600

    Cleaned up code - added Optional Object for findAllById

--- DD-Creator/src/main/java/com/example/demo/Blog/repository/PostRepository.java
+++ DD-Creator/src/main/java/com/example/demo/Blog/repository/PostRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 // interface for handling Post entity in database
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findByKeyword(String lowercaseKeyword);
+
+    Optional<Object> findAllById(Long id);
 }
