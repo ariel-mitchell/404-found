f72263ba08f23e2a08dfb8bf9086838c191af308
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-18 00:22:25 -0600

    Added Comments for better readability

--- DD-Creator/src/main/java/com/example/demo/Blog/service/PostService.java
+++ DD-Creator/src/main/java/com/example/demo/Blog/service/PostService.java
@@ -13,7 +13,7 @@ public class PostService {
     private final PostRepository postRepository;
 
     @Autowired
-    private PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
 
@@ -21,26 +21,34 @@ public class PostService {
     public List<Post> getAllPosts() {
         return postRepository.findAll();
     }
-    // Get by keyword
+
+    // Get posts by keyword
     public List<Post> getPostsByKeyword(String lowercaseKeyword) {
+        // TODO: Implement logic to search for posts by keyword
         return null;
     }
+
     //Get a post by ID
     public Post getPostById(Long id) {
-        //Post doesn't exist
-        return postRepository.findById(id).orElse(null);
+        // Throws PostNotFoundException if the post is not found
+        return postRepository.findById(id)
+                .orElseThrow(() -> new PostNotFoundException("Post not found with id: " + id));
     }
+
     //Create a new post
     public Post createPost(Post post) {
-        //Save new post and return it
+        // Save new post and return it
         return postRepository.save(post);
     }
+
     //Delete post by ID
     public void deletePost(Long id) {
-        //Delete the post if it exists - void return type
+        // Delete the post if it exists - void return type
         if (postRepository.existsById(id)) {
             postRepository.deleteById(id);
         }
     }
+
     //Like and Unlike Post
 }
+
