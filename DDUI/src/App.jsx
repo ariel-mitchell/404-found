e59f30ce6cea63b776e086599869bf334903c44e
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-17 21:12:48 -0600

    added basic css styling

--- DDUI/src/App.jsx
+++ DDUI/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import "./BlogComponents";
 import PostList from './BlogComponents/PostLists';
 import CreatePost from './BlogComponents/CreatePost';
 
 function App() {
-return (
-    <div className="App">
-    <h1>Blog App</h1>
-    <CreatePost />
-    <PostList />
+    return (
+    <div className="App container">
+        <h1>Blog App</h1>
+        <CreatePost className="create-post-form" />
+        <PostList className="post-list" />
     </div>
     );
 }
