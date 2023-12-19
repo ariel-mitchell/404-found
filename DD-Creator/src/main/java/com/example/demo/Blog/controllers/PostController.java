7f0683c59b9eae04a591730fb14b9ceca4dd3b44
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2023-12-18 21:34:56 -0600

    Cleaned up code

--- DD-Creator/src/main/java/com/example/demo/Blog/controllers/PostController.java
+++ DD-Creator/src/main/java/com/example/demo/Blog/controllers/PostController.java
@@ -1,17 +1,19 @@
 package com.example.demo.Blog.controllers;
 
+import com.example.demo.Blog.models.Comment;
 import com.example.demo.Blog.models.Post;
+import com.example.demo.Blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.example.demo.Blog.service.PostService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+
     private final PostService postService;
 
     @Autowired
@@ -33,10 +35,38 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdPost);
     }
 
+    // Update Post
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post updatedPost) {
+        Post post = postService.updatePost(id, updatedPost);
+        return ResponseEntity.ok(post);
+    }
+
+    // Add Comment to Post
+    @PostMapping("/{postId}/comments")
+    public ResponseEntity<Comment> addComment(@PathVariable Long postId, @RequestBody Comment comment) {
+        Comment addedComment = postService.addComment(postId, comment);
+        return ResponseEntity.status(HttpStatus.CREATED).body(addedComment);
+    }
+
+    // Get Comments for Post
+    @GetMapping("/{postId}/comments")
+    public ResponseEntity<List<Comment>> getCommentsForPost(@PathVariable Long postId) {
+        List<Comment> comments = postService.getCommentsForPost(postId);
+        return ResponseEntity.ok(comments);
+    }
+
     // Delete Post
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Long id) {
         postService.deletePost(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Search Posts by Keyword
+    @GetMapping("/search")
+    public ResponseEntity<List<Post>> searchPosts(@RequestParam String keyword) {
+        List<Post> posts = postService.getPostsByKeyword(keyword);
+        return ResponseEntity.ok(posts);
     }
 }
