f7625564db5a9c9b0f9b79da2fb7c237ea27c10e
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-03 18:45:31 -0600

    Category Entity

---
+++ DnDCreator/src/main/java/models/Category.java
@@ -0,0 +1,51 @@
+package models;
+
+import jakarta.persistence.Id;
+
+import java.util.List;
+
+public class Category {
+
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+
+    private List<Post> posts;
+
+    public Category() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+}
