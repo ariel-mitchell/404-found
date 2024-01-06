40d1e477bd7b091f05443d7982c1512dbc5ac8ac
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-06 14:16:12 -0600

    Add useEffect and getSpells function

--- DnDCreator/src/main/DDUI/build/App.jsx
+++ DnDCreator/src/main/DDUI/build/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { getSpells } from './components/api'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [spells, setSpells] = useState([]);
 
   return (
     <>
