4abd3a0e4e098305b45268b5e32a1861c92b3085
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-06 13:39:38 -0600

    React test

--- DnDCreator/src/main/DDUI/build/App.jsx
+++ DnDCreator/src/main/DDUI/build/App.jsx
@@ -16,13 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR
+        <p>Edit App.jsx and save to test HMR updates.
         </p>
       </div>
       <p className="read-the-docs">
