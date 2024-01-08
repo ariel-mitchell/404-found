a3ef1c61f9521a748f266c02f3cf5f342cfcbe3d
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-07 21:00:09 -0600

    Fix API call and add spell details retrieval

--- DnDCreator/src/main/DDUI/build/components/api.jsx
+++ DnDCreator/src/main/DDUI/build/components/api.jsx
@@ -2,9 +2,17 @@ const BASE_URL = "https://www.dnd5eapi.co"
 
 export async function getSpells() {
     const response = await fetch(`${BASE_URL}/api/spells`);
+    const spells = await response.json();
     return await response.json();
+
+    const spellDetails await Promise.all( 
+        spells.results.map((spells) => fetch(`${BASE_URL}${spells.url}`).then((res) => res.json()))
+    );
 }
 export async function getClasses() {
     const response = await fetch(`${BASE_URL}/api/classes`);
     return await response.json();
 }
+
+return spellDetails;
+
