3b91e7821487525c0e60e94c64fdd55514a2cd58
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-07 21:01:02 -0600

    Refactor API.jsx to use getAllSpells function

--- DnDCreator/src/main/DDUI/build/components/api.jsx
+++ DnDCreator/src/main/DDUI/build/components/api.jsx
@@ -1,18 +1,14 @@
-const BASE_URL = "https://www.dnd5eapi.co"
+const BASE_URL = 'https://www.dnd5eapi.co';
 
-export async function getSpells() {
-    const response = await fetch(`${BASE_URL}/api/spells`);
-    const spells = await response.json();
-    return await response.json();
+export async function getAllSpells() {
+const response = await fetch(`${BASE_URL}/api/spells`);
+const spells = await response.json();
 
-    const spellDetails await Promise.all( 
-        spells.results.map((spells) => fetch(`${BASE_URL}${spells.url}`).then((res) => res.json()))
-    );
-}
-export async function getClasses() {
-    const response = await fetch(`${BASE_URL}/api/classes`);
-    return await response.json();
-}
+
+const spellDetails = await Promise.all(
+    spells.results.map((spell) => fetch(`${BASE_URL}${spell.url}`).then((res) => res.json()))
+);
 
 return spellDetails;
+}
 
