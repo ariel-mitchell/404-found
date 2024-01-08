64ca16683e8740e3f49095bdc7955a53db1a04ed
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-07 20:43:54 -0600

    Refactor SpellList component

--- DnDCreator/src/main/DDUI/build/components/SpellList.jsx
+++ DnDCreator/src/main/DDUI/build/components/SpellList.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect, useState} from "react";
-import { getAllSpells} from './api'
+import React, { useEffect, useState } from 'react';
+import { getAllSpells } from './api';
 import SpellCard from './SpellCard';
 
 function SpellList() {
-    const {spells, setSpells} = useState([]);
+const [spells, setSpells] = useState([]);
 
-    useEffect(() => {
-        getAllSpells().then((spells) => {
-            setSpells(spells);
-            localStorage.setItem('spells', JSON.stringify(spells));
-        });
+useEffect(() => {
+    getAllSpells().then((spells) => {
+    setSpells(spells);
+    localStorage.setItem('spells', JSON.stringify(spells));
     });
+}, []);
+return (
+    <div className='App'>
+        {spells.length === 0 && <span className='loading'>Loading...</span>}
+        <ul className='spells-list'>
+            {spells.map((spell) => (
+                    <SpellCard key={spell.index} spell={spell} />
+                ))}    
+        </ul>    
+        </div>
+    );
 }
+
+export default SpellList;
