9da8a79138a40ffadd9fa6f1173cc3be9798f736
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-07 21:06:39 -0600

    Refactor index.jsx file

--- DnDCreator/src/main/DDUI/build/index.jsx
+++ DnDCreator/src/main/DDUI/build/index.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './Main.jsx'; // Import your Main component
 
-function App() {
-  const [count, setCount] = useState(0)
+const rootElement = document.getElementById('root');
 
-  return (
-    <div>
-      {/* Add your JSX code here */}
-    </div>
-  )
-}
-export default App
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+  rootElement
+);
