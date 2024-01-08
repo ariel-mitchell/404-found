31560c166b63a67a4d6f3384af08eb78aab50720
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-08 15:45:04 -0600

    Add styling to Home component

--- DnDCreator/src/main/DDUI/build/components/Home.jsx
+++ DnDCreator/src/main/DDUI/build/components/Home.jsx
@@ -1,13 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+
+import React from 'react';
+import "styles.css";
 
 function Home() {
 return (
-    <div>
-    <h1>Home</h1>
-    <Link to="/spells">View Spells</Link>
+    <div className="container">
+    <div className="card">
+        <h1>Home</h1>
+        {/* Other content */}
     </div>
-    );
+    </div>
+);
 }
 
 export default Home;
