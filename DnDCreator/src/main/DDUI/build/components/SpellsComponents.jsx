83ce7ded2ffe70ca1c377bb7c9d64f9dd6b5e80c
Julesmcghouls <132119772+Julesmcghouls@users.noreply.github.com>
2024-01-07 21:51:42 -0600

    Add Home component to index.jsx

--- DnDCreator/src/main/DDUI/build/components/SpellsComponents.jsx
+++ DnDCreator/src/main/DDUI/build/components/SpellsComponents.jsx
@@ -48,4 +48,4 @@ const AddSpellsForm = ({ onAddSpell }) => {
   );
 };
 
-export default AddSpellsForms;
+export default AddSpellsForm;
--- DnDCreator/src/main/DDUI/build/index.jsx
+++ DnDCreator/src/main/DDUI/build/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main.jsx';
+import Home from "./components/Home.jsx";
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
     <Main />
+    <Home />
   </React.StrictMode>,
   rootElement
 );
